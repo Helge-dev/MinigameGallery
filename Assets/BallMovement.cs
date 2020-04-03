9db9b6932f2df4c02e261372db7a70f359b85f08
Arvid-00 <62692818+Arvid-00@users.noreply.github.com>
2020-04-03 13:46:58 +0200

    Created Basic Table Tennis

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public bool buttonState = true;
+    void Start()
+    {
+        
+        //rb.AddForce(1500, 500, 0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("h") && buttonState == true)
+        {
+            rb.AddForce(1000 , 800 , 0);
+            buttonState = false;
+        }
+    }
+}
