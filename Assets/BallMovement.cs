84fc56e539c01d799bea48e6bf34dd9523a54259
Arvid-00 <62692818+Arvid-00@users.noreply.github.com>
2020-04-14 12:17:32 +0200

    Nästan spelbar table tennis

--- Assets/BallMovement.cs
+++ Assets/BallMovement.cs
@@ -1,23 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class BallMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public bool buttonState = true;
+    public bool canHit = true;
+
+    public Material[] material;
+    Renderer rend;
+
+
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[1];
         //rb.AddForce(1500, 500, 0);
     }
-
     void Update()
     {
-        if (Input.GetKey("h") && buttonState == true)
+        if ((rb.position.x <= -50 || rb.position.x >= 50) && canHit == false)
         {
-            rb.AddForce(1000 , 800 , 0);
-            buttonState = false;
+            rb.velocity = Vector3.zero;
+            rb.useGravity = false;
+            buttonState = true;
+            canHit = true;
+            rend.sharedMaterial = material[1];
+            
         }
+        if(rb.position.x < 50 && rb.position.x > -50)
+        {
+            canHit = false;
+        }
+
+
+        if (Input.GetKey("1") && buttonState == true)
+        {
+            if(rb.position.x <= -50)
+                rb.AddForce(1200 , 800 , 0);
+            else
+                rb.AddForce(-1200, 800, 0);
+            rb.useGravity = true;
+            buttonState = false;
+            rend.sharedMaterial = material[0]; 
+        }
+        if (Input.GetKey("2") && buttonState == true)
+        {
+            if (rb.position.x <= -50)
+                rb.AddForce(800, 1200, 0);
+            else
+                rb.AddForce(-800, 1200, 0);
+            rb.useGravity = true;
+            buttonState = false;
+            rend.sharedMaterial = material[0];
+
+            
+        }
+        if (Input.GetKey("3") && buttonState == true)
+        {
+            if (rb.position.x <= -50)
+                rb.AddForce(2000, 0, 0);
+            else
+                rb.AddForce(-2000, 0, 0);
+            rb.useGravity = true;
+            buttonState = false;
+            rend.sharedMaterial = material[0];
+        }
+
     }
 }
