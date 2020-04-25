f28f2046d7b075db2fe8f7805410bd066cd5f6c9
Helge-dev <62693004+Helge-dev@users.noreply.github.com>
2020-04-25 13:55:28 +0200

    WIP Player Spawn System

---
+++ Assets/Scripts/WaterWar/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] PlayerSpawnMananger psm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Show Game Instructions
+        //Create Map
+        //Spawn Players
+        psm.CreatePlayers();
+    }
+}
---
+++ Assets/Scripts/WaterWar/PlayerScripts/PlayerSpawnMananger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+/*
+ * Created by Helge Herrström 
+ */
+public class PlayerSpawnMananger : MonoBehaviour
+{
+    [SerializeField] GameObject playerObject;
+    [SerializeField] Transform playerTransform;
+    /// <summary>
+    /// Creates a player for each controller registered
+    /// </summary>
+    public void CreatePlayers()
+    {
+        for (int i = 0; i < DataStorage.GetSetControllers.Count-1; i++)
+        {
+            Instantiate(playerObject, playerTransform);
+        }
+    }
+}
